ad8e74f89d3d9efeaba7fd606c796d98bf380d02
Jonathan Yuan <jy90023@gmail.com>
2022-03-28 21:07:02 -0400

    create and style navbar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import './styles/Reset.css';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            activeTab: 'Personal',
+        };
+
+        this.changeTab = this.changeTab.bind(this);
+    }
+
+    changeTab(e) {
+        this.setState({
+            activeTab: e.currentTarget.textContent,
+        });
+    }
+
+    render() {
+        const { activeTab } = this.state;
+        return (
+            <>
+                <Nav activeTab={activeTab} tabHandler={this.changeTab} />
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../styles/Nav.css';
+
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { tabHandler, activeTab } = this.props;
+        return (
+            <nav id={'navbar'}>
+                <ul id={'nav-links'}>
+                    <li
+                        className={`${
+                            activeTab === 'Personal' ? 'active-tab ' : ''
+                        }tab-link`}
+                        onClick={tabHandler}
+                    >
+                        Personal
+                    </li>
+                    <div className={'link-divider'}></div>
+                    <li
+                        className={`${
+                            activeTab === 'Work' ? 'active-tab ' : ''
+                        }tab-link`}
+                        onClick={tabHandler}
+                    >
+                        Work
+                    </li>
+                    <div className={'link-divider'}></div>
+                    <li
+                        className={`${
+                            activeTab === 'Education' ? 'active-tab ' : ''
+                        }tab-link`}
+                        onClick={tabHandler}
+                    >
+                        Education
+                    </li>
+                    <div className={'link-divider'}></div>
+                    <li
+                        className={`${
+                            activeTab === 'Skills' ? 'active-tab ' : ''
+                        }tab-link`}
+                        onClick={tabHandler}
+                    >
+                        Skills
+                    </li>
+                </ul>
+                <button id="generate-pdf-btn">Generate pdf</button>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
