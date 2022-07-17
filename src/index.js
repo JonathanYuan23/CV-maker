e61d6e5eb3e61ee78f224970b3cfdcf626856ac5
Jonathan Yuan <jy90023@gmail.com>
2022-07-17 17:55:50 -0400

    add firebase config object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Viewer } from './utils/PDFDocument';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -9,5 +8,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// ReactDOM.render(<Viewer />, document.getElementById('root'));
